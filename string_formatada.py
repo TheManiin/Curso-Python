f89974a9c75db2592e7e00cd5825e676f11f166a
TheManiin <168486549+TheManiin@users.noreply.github.com>
2024-05-10 14:16:33 -0300

    Add files via upload

---
+++ string_formatada.py
@@ -0,0 +1,10 @@
+nome = 'Felipe'
+sobrenome = 'Ferreira'
+profissao = 'Programador'
+
+texto = 'O ' + nome + ' ' + sobrenome + ' é um excelente ' + '[' + profissao + ']'
+
+texto2 = f'O {nome} {sobrenome} é um excelente [{profissao}]'
+
+print(texto)
+print(texto2)
