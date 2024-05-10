4e0fc843322030e63356148e41133105fed70c15
TheManiin <168486549+TheManiin@users.noreply.github.com>
2024-05-09 22:31:43 -0300

    Add files via upload

---
+++ operador_ternario.py
@@ -0,0 +1,12 @@
+# Operador Ternário
+
+idade = 17
+
+# if idade >= 16:
+#    resultado = print('Voto permitido')
+# else:
+#    resultado = print('Voto não permitido')
+
+resultado = 'Voto Permitido' if idade >= 16 else 'Voto não permitido'
+
+print(resultado)
