f1d6a32b0ab2b72bf43013033cdb8af58bbd0ce6
TheManiin <168486549+TheManiin@users.noreply.github.com>
2024-05-09 22:21:23 -0300

    Add files via upload

---
+++ for_separando_string.py
@@ -0,0 +1,11 @@
+# For loop nested (Loopinng)
+
+   #Outer loop 
+     #Inner loop
+
+# Modificar de FANTASTICO para F A N T A S T I C O
+
+palavra = 'FANTASTICO'
+
+for spaco in palavra:
+    print(f' {spaco}' , end='')
