48d0e693353a52c59eff23d2a879614f0f5b0ab2
TheManiin <168486549+TheManiin@users.noreply.github.com>
2024-05-09 22:14:28 -0300

    Add files via upload

---
+++ for_if_else.py
@@ -0,0 +1,15 @@
+# For loop (Loopinng)
+
+# For Utilizando If e Else
+
+compra_confirmada = True
+dados_compra = 'Compra no valor de R$12.50 e entrega confirmada'
+
+
+for enviar in range(3):
+  if compra_confirmada:
+      print(dados_compra)
+      print('Detalhes enviados para o seu email')
+      break
+else:
+  print('Falha na compra')
