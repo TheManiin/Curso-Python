5a0610a9e4c7755b535ad34ed648188663339517
TheManiin <168486549+TheManiin@users.noreply.github.com>
2024-05-09 22:29:03 -0300

    Add files via upload

---
+++ operadores_comparacao.py
@@ -0,0 +1,10 @@
+# Multiplicos Operadores de Compração
+
+valor = 39
+
+if 20 <= valor <40:
+
+# if valor >= 20 and valor < 40:
+    print('Produto foi aceito')
+else:
+    print('Produto não aceito')
